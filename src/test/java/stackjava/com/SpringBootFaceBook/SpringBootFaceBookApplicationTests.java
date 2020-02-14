8d4c17500dc35afd2538473204e092d9e5421f65
quat <hongocquat0412@gmail.com>
2020-02-14 16:57:15 +0700

    fb

---
+++ src/test/java/stackjava/com/SpringBootFaceBook/SpringBootFaceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package stackjava.com.SpringBootFaceBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFaceBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
